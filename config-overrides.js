ca0ea059ba8354d8755234e77195f2ed86418e3a
mateja <mateja176@gmail.com>
2019-05-06 14:40:42 +0200

    adds config.overrides.js

---
+++ config-overrides.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+
+module.exports = (config, env) => rewireReactHotLoader(config, env);
