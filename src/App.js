66217783d436d04403b38f5a22f1fe486e734b91
mateja <mateja176@gmail.com>
2019-05-06 14:45:24 +0200

    tests out hot reloading in App.js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Create React App with Hot Reloading</h1>
+        <h2>Thanks to</h2>
+        <ul>
+          <li>
+            <a href="https://github.com/cdharris/react-app-rewire-hot-loader">
+              React Hot Loader
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/cdharris/react-app-rewire-hot-loader">
+              React App Rewired
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/cdharris/react-app-rewire-hot-loader">
+              React App Rewire Hot Loader
+            </a>
+          </li>
+        </ul>
       </header>
     </div>
   );
